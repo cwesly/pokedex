a415703f1d4f97be73865089a4417303db45f325
Wesleysoares7 <wesleysoares7@yahoo.com.br>
2024-05-25 17:12:47 -0300

    feat: create search logic

--- src/searchBar/searchBar.tsx
+++ src/searchBar/searchBar.tsx
@@ -1,14 +1,21 @@
-import { FaSearch } from "react-icons/fa"
+import { FaSearch } from "react-icons/fa";
 type SearchBarProps = {
-    search: string
-    setSearch: React.Dispatch<React.SetStateAction<string>>
-}
-const SearchBar = ({search, setSearch}: SearchBarProps) => {
-    return (
-        <div>
-            <input type="text" value={search} onChange={(event) => setSearch(event.target.value)}/>
-            <button><FaSearch /></button>
-        </div>
-    )
-}
-export default SearchBar
+  search: string;
+  setSearch: React.Dispatch<React.SetStateAction<string>>;
+  fetchPokemon: () => Promise<void>;
+};
+const SearchBar = ({ search, setSearch, fetchPokemon }: SearchBarProps) => {
+  return (
+    <div>
+      <input
+        type="text"
+        value={search}
+        onChange={(event) => setSearch(event.target.value)}
+      />
+      <button onClick={() => fetchPokemon()}>
+        <FaSearch />
+      </button>
+    </div>
+  );
+};
+export default SearchBar;
