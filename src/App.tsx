a9f44bd35b05032c72b011cd8d105e7822e940ed
josuejcs13 <josuejcs13@gmail.com>
2024-05-25 15:39:09 -0400

    feat: add fetch pokemon function

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,28 @@
-import { useState } from "react";
-import Header from "./header/header";
+import { useEffect, useState } from "react"
+import Header from "./header/header"
+import { Pokemon } from "./types"
 
 function App() {
-  const [search, setSearch] = useState<string>("");
+  const [search, setSearch] = useState<string>("")
+  const [pokemon, setPokemon] = useState<Pokemon>({ name: "", id: 0 })
+  useEffect(() => {
+    const fetchPokemon = async () => {
+      const data = await fetch("https://pokeapi.co/api/v2/pokemon/25")
+      const pokemonData = await data.json()
+      setPokemon(pokemonData)
+    }
+    fetchPokemon()
+  }, [])
   return (
     <>
       <Header
-        id={1}
-        pokemonName="Bulbasaur"
+        id={pokemon.id}
+        pokemonName={pokemon.name}
         search={search}
         setSearch={setSearch}
       />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
