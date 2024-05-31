7f21c1aa157cccf4226de8f0ab9ee2f4f2c3d7c1
josuejcs13 <josuejcs13@gmail.com>
2024-05-31 19:31:51 -0400

    style: add image style

--- src/imagePokemon/imagePokemon.tsx
+++ src/imagePokemon/imagePokemon.tsx
@@ -1,10 +1,12 @@
+import "./imagePokemon.css";
+
 type ImagePokemonProps = {
   id: number;
 };
 
 function ImagePokemon({ id }: ImagePokemonProps) {
   const imagePokemon = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`;
-  return <img src={imagePokemon} alt="Image of selected Pokémon"></img>;
+  return <img src={imagePokemon} alt="Image of selected Pokémon" />;
 }
 
 export default ImagePokemon;
