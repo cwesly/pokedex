4a6347637eecc87e031efa5d4a9d3c1d6c8d4dec
josuejcs13 <josuejcs13@gmail.com>
2024-06-01 14:29:32 -0400

    fix: remove api call from pokemonTypeTag

--- src/pokemonTypeTag/pokemonTypeTag.tsx
+++ src/pokemonTypeTag/pokemonTypeTag.tsx
@@ -1,21 +1,14 @@
-import { useEffect, useState } from "react";
-
-type PokemonTypeProp = {
-  id: number;
+import "./pokemonTypeTag.css";
+type PokemonTypeTagProp = {
+  name: string;
 };
 
-const PokemonType = ({ id }: PokemonTypeProp) => {
-  const [pokemonType, setPokemonType] = useState<string>("");
-
-  useEffect(() => {
-    const fetchPokemonType = async () => {
-      const data = await fetch(`https://pokeapi.co/api/v2/type/${id}/`);
-      const pokemonTypeData = await data.json();
-      setPokemonType(pokemonTypeData.name);
-    };
-    fetchPokemonType();
-  }, [id]);
-  return <h2>{pokemonType}</h2>;
+const PokemonTypeTag = ({ name }: PokemonTypeTagProp) => {
+  return (
+    <div className="tag-type" style={{ background: `var(--${name}-color)` }}>
+      <p>{name}</p>
+    </div>
+  );
 };
 
-export default PokemonType;
+export default PokemonTypeTag;
