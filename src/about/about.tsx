635baf287525acd69060aa9d684531f6d3386184
josuejcs13 <josuejcs13@gmail.com>
2024-06-05 20:45:00 -0400

    fix: prop not used

--- src/about/about.tsx
+++ src/about/about.tsx
@@ -1,25 +1,25 @@
 import { Pokemon } from "../types";
 import { GiWeight } from "react-icons/gi";
 import { TfiRuler } from "react-icons/tfi";
-
+import "./about.css";
 type AboutProp = {
   pokemon: Pokemon;
 };
 
-const About = ({ pokemon: { weight, height, stats } }: AboutProp) => {
+const About = ({ pokemon: { weight, height } }: AboutProp) => {
   const weightKg = weight * 0.1;
   const heightMt = height * 0.1;
   return (
-    <div>
+    <div className="about">
       <h3>About</h3>
-      <div>
+      <div className="about-ifo">
         <div>
           <GiWeight />
-          <p>{weightKg.toFixed(2)} Kg</p>
+          <p>{weightKg.toFixed(1)} Kg</p>
         </div>
         <div>
           <TfiRuler />
-          <p>{heightMt.toFixed(2)} M</p>
+          <p>{heightMt.toFixed(1)} M</p>
         </div>
       </div>
     </div>
